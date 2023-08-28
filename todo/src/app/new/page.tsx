54e8ebb4400bc10265e9ccb4b9d6445a6cf528a1
Daniil Kravchenko <daniil.kravchenko.92@yandex.ru>
2023-08-28 17:44:44 +0200

    code along completed

--- todo/src/app/new/page.tsx
+++ todo/src/app/new/page.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import { prisma } from "@/db"
+import { redirect } from 'next/navigation'
 
+async function createTodo(data: FormData){
+  "use server"
+  const title = data.get("title")?.valueOf()
+  if(typeof title !== "string" || title.length === 0){
+    throw new Error("Invalid Title")
+  }
+
+  await prisma.todo.create({data: {title, complete: false}})
+  redirect("/")
+}
 export default function Page() {
   return (
-    <div>
-      new
-    </div>
+    <>
+     <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+      <form action={createTodo} className='flex gap-2 flex-col'>
+        <input type='text' name='title'></input>
+        <div className='flex gap-1 justify-end'>
+          <Link href="..">Cancel</Link>
+          <button type="submit">Create</button>
+        </div>
+      </form>
+    </>
   )
 }
--- todo/src/app/page.tsx
+++ todo/src/app/page.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import Link from "next/link"
+import { prisma } from "@/db"
+import TodoItem from "@/components/TodoItem"
 
-function Home() {
+function getTodos(){
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean ){
+  "use server"
+  await prisma.todo.update({ where : {id}, data: {complete}})
+}
+
+async function Home() {
+  const todos = await getTodos()
   return (
     <>
-      <header>
-        <h1>Todos</h1>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link href="/new">New</Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}></TodoItem>
+        ))}
+      </ul>
     </>
   )
 }
